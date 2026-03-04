52b4d7cf43e164f0e1154b711efae15125fa2865
Apurvasri25 <apurvasrikanakasabapathi@gmail.com>
2026-03-04 11:49:41 +0530

    Time: 7 ms (42.84%), Space: 45.3 MB (64.03%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -5,12 +5,9 @@ class Solution {
         Stack<Integer> stck = new Stack<>();
 
         for (String s : tokens) {
-            // Check if the token is an operator
             if ("+-*/".contains(s) && s.length() == 1) {
-                // The second operand is popped first (LIFO)
                 int n2 = stck.pop();
                 int n1 = stck.pop();
-                
                 switch (s) {
                     case "+": stck.push(n1 + n2); break;
                     case "-": stck.push(n1 - n2); break;
@@ -18,11 +15,9 @@ class Solution {
                     case "/": stck.push(n1 / n2); break;
                 }
             } else {
-                // It's a number, convert and push onto stck
                 stck.push(Integer.parseInt(s));
             }
         }
-        // The final result is the only thing left in the stck
         return stck.pop();
     }
 }
