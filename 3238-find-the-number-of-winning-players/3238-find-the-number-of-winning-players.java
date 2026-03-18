a8b4808387a23f3b34200f4213ff02f6abacbb3d
Apurvasri25 <apurvasrikanakasabapathi@gmail.com>
2026-03-18 21:46:33 +0530

    Time: 4 ms (42.21%), Space: 46.9 MB (86.4%) - LeetHub

---
+++ 3238-find-the-number-of-winning-players/3238-find-the-number-of-winning-players.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int winningPlayerCount(int n, int[][] pick) {
+        int winner = 0;
+        // Map: Player ID -> (Ball Color -> Count)
+        HashMap<Integer, HashMap<Integer, Integer>> map = new HashMap<>(); 
+        HashSet<Integer> playerWinner = new HashSet<>();
+
+        for(int i = 0; i < pick.length; i++){
+            int player = pick[i][0];
+            int color = pick[i][1];
+
+            // If player has already won, no need to check further
+            if(playerWinner.contains(player)) continue;
+
+            HashMap<Integer, Integer> ballDetails = map.getOrDefault(player, new HashMap<>());
+            ballDetails.put(color, ballDetails.getOrDefault(color, 0) + 1);
+            map.put(player, ballDetails);
+
+            // Winning condition: count of balls of same color > player index
+            if(ballDetails.get(color) > player){
+                winner++;
+                playerWinner.add(player);
+            }
+        }
+        return winner;
+    }
+}
