59a8a738c2e123661bdd924cad542c497f69f3c0
Apurvasri25 <apurvasrikanakasabapathi@gmail.com>
2026-08-07 16:09:15 +0530

    Time: 19 ms (10.39%), Space: 46.8 MB (5.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,39 +1,37 @@
 class Solution {
-    /**
-     * Determines if two strings are anagrams of each other.
-     * Two strings are anagrams if they contain the same characters with the same frequencies.
-     * 
-     * @param s the first string to compare
-     * @param t the second string to compare
-     * @return true if the strings are anagrams, false otherwise
-     */
+
     public boolean isAnagram(String s, String t) {
-        // Early return if strings have different lengths - they cannot be anagrams
+
         if (s.length() != t.length()) {
             return false;
         }
-      
-        // Array to track character frequency differences
-        // Index represents character (0 for 'a', 1 for 'b', ..., 25 for 'z')
-        int[] characterCount = new int[26];
-      
-        // Process both strings simultaneously
-        // Increment count for characters in string s
-        // Decrement count for characters in string t
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // Count characters of first string
         for (int i = 0; i < s.length(); i++) {
-            characterCount[s.charAt(i) - 'a']++;
-            characterCount[t.charAt(i) - 'a']--;
+
+            char ch = s.charAt(i);
+
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-      
-        // Check if all character counts are zero
-        // If any count is non-zero, the strings are not anagrams
-        for (int i = 0; i < 26; i++) {
-            if (characterCount[i] != 0) {
+
+        // Reduce count using second string
+        for (int i = 0; i < t.length(); i++) {
+
+            char ch = t.charAt(i);
+
+            if (!map.containsKey(ch)) {
                 return false;
             }
+
+            map.put(ch, map.get(ch) - 1);
+
+            if (map.get(ch) == 0) {
+                map.remove(ch);
+            }
         }
-      
-        // All character counts are zero, strings are anagrams
-        return true;
+
+        return map.isEmpty();
     }
 }
